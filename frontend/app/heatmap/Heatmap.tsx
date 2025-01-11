05181370080e19cd3147a6e11352b50f208cb770
Om Mahesh <2om.mahesh@gmail.com>
2025-01-11 14:55:44 -0800

    made CourtHeatmap component

--- frontend/app/heatmap/Heatmap.tsx
+++ frontend/app/heatmap/Heatmap.tsx
@@ -10,23 +10,23 @@ interface DataPoint {
   value: number;
 }
 
-const Heatmap = ({ data, width }: { data: DataPoint[]; width: number }) => {
-  const heatmapContainer = useRef(null); // Reference to the heatmap container
-
-  useEffect(() => {
-    // Initialize Heatmap.js
-    const heatmapInstance = h337.create({
-      container: heatmapContainer.current,
-      radius: 50, // Adjust radius for the heatmap points
-    });
-
-    const max = Math.max(...data.map((o) => o.value));
-
-    // Add data to the heatmap
-    heatmapInstance.setData({ max, data });
-  }, [data]); // Re-run the effect when data changes
-
+export const CourtHeatmap = ({
+  lData,
+  rData,
+  radius,
+  width,
+}: {
+  lData: DataPoint[];
+  rData: DataPoint[];
+  radius: number;
+  width: number;
+}) => {
   const height = Math.round((350 / 612) * width);
+  const x_margin = 26.5;
+  const y_margin = 21.5;
+  const courtWidth = width / 2 - x_margin;
+  const courtHeight = height - 2 * y_margin;
+
   return (
     <div
       className="flex relative"
@@ -46,16 +46,73 @@ const Heatmap = ({ data, width }: { data: DataPoint[]; width: number }) => {
           height: `${height}px`,
         }}
       />
-      <div
-        ref={heatmapContainer}
-        className="absolute"
-        style={{
-          width: "100%",
-          height: `${height}px`, // Ensure heatmap container takes the same height
-        }}
+      <Heatmap
+        data={lData}
+        radius={radius}
+        width={courtWidth}
+        height={courtHeight}
+        left={x_margin}
+        top={y_margin}
+      />
+      <Heatmap
+        data={rData}
+        radius={radius}
+        width={courtWidth}
+        height={courtHeight}
+        left={x_margin}
+        top={y_margin}
       />
     </div>
   );
 };
 
-export default Heatmap;
+const Heatmap = ({
+  data,
+  radius,
+  width,
+  height,
+  left,
+  top,
+}: {
+  data: DataPoint[];
+  radius: number;
+  width: number;
+  height: number;
+  left: number;
+  top: number;
+}) => {
+  const heatmapContainer = useRef(null); // Reference to the heatmap container
+
+  data = data.map(({ x, y, value }) => ({
+    x: Math.round((x / 100) * width),
+    y: Math.round((y / 100) * height),
+    value,
+  }));
+
+  useEffect(() => {
+    // Initialize Heatmap.js
+    const heatmapInstance = h337.create({
+      container: heatmapContainer.current,
+      radius, // Adjust radius for the heatmap points
+    });
+
+    // const max = Math.max(...data.map((o) => o.value));
+    const max = 10;
+
+    // Add data to the heatmap
+    heatmapInstance.setData({ max, data });
+  }, [data]); // Re-run the effect when data changes
+
+  return (
+    <div
+      ref={heatmapContainer}
+      className="absolute"
+      style={{
+        width: `${width}px`,
+        height: `${height}px`,
+        left: `${left}px`,
+        top: `${top}px`,
+      }}
+    />
+  );
+};
--- frontend/app/heatmap/page.tsx
+++ frontend/app/heatmap/page.tsx
@@ -1,17 +1,49 @@
-import Heatmap from "./Heatmap";
+import { CourtHeatmap } from "./Heatmap";
+
+// class Point {
+//   constructor() {
+//     this.curr = {
+//       x: Math.round(Math.random() * 100),
+//       y: Math.round(Math.random() * 100),
+//     };
+//   }
+//   move() {
+//     const dist = 10;
+//     this.curr.x += Math.round(Math.random() * dist * 2 - dist);
+//     this.curr.x = Math.max(Math.min(this.curr.x, 100), 0);
+//     this.curr.y += Math.round(Math.random() * dist * 2 - dist);
+//     this.curr.y = Math.max(Math.min(this.curr.y, 100), 0);
+//   }
+//   get() {
+//     return {
+//       x: this.curr.x,
+//       y: this.curr.y,
+//       value: Math.round(Math.random() * 10),
+//     };
+//   }
+// }
 
 export default function Home() {
+  // rng points
+  const lData = [];
+  const rData = [];
+  for (let i = 0; i < 50; i++) {
+    lData.push({
+      x: Math.round(Math.random() * 100),
+      y: Math.round(Math.random() * 100),
+      value: Math.round(Math.random() * 10),
+    });
+    rData.push({
+      x: Math.round(Math.random() * 100),
+      y: Math.round(Math.random() * 100),
+      value: Math.round(Math.random() * 10),
+    });
+  }
+  // import data here prob
   return (
     <>
       <div>heatmap page</div>
-      <Heatmap
-        data={[
-          { x: 10, y: 15, value: 5 },
-          { x: 100, y: 105, value: 9 },
-          { x: 100, y: 105, value: 9 },
-        ]}
-        width={500}
-      />
+      <CourtHeatmap lData={lData} rData={rData} radius={20} width={300} />
     </>
   );
 }
